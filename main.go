8e7c7bfd65cf53c282e2d2183927178376ebb2f5
moms-spaghetti <38544356+moms-spaghetti@users.noreply.github.com>
2023-02-14 02:39:58 +0000

    Update main.go

--- main.go
+++ main.go
@@ -6,50 +6,50 @@ import (
 )
 
 var (
-	ch    = make(chan int)
-	done  = make(chan struct{}, 1)
-	token = make(chan struct{}, 1)
-	wait  = make(chan struct{}, 1)
+	data  = make(chan int)         // data used to send to processor routine from main
+	done  = make(chan struct{}, 1) // empty chan if closed kills processor
+	token = make(chan struct{}, 1) // holds tokens to be read when needing to stop executing a function
+	wait  = make(chan struct{}, 1) // empty chan if close kills main routine
 )
 
 func main() {
-	setupTokens()
+	setupTokens() // add tokens to token chan
 
-	go processor()
+	go processor() // start processor separate routine
 
 	send()
-	<-wait
+	<-wait // blocks main
 	fmt.Println("main finsihed")
 }
 
 func send() {
 	fmt.Println("send send started...")
 
-	ch <- 10
+	data <- 10 // send int to data chan
 	fmt.Println("send waiting...")
-	<-token
+	<-token // blocks send() processing as struct has been read and token chan is empty (reading from empty chan = block)
 
 	fmt.Println("send finished...")
-	close(done)
-	close(ch)
-	close(wait)
+	close(done) // kill processor first so not reading from still open data chan
+	close(data) // now close data
+	close(wait) // close wait to allow main routine to continue processing
 }
 
 func processor() {
 	for {
 		select {
-		case n := <-ch:
+		case n := <-data: // receives into processor from send
 
-			time.Sleep(3 * time.Second)
-			fmt.Println("processed number: ", n*10)
-			token <- struct{}{}
+			time.Sleep(3 * time.Second)             // simulate processing
+			fmt.Println("processed number: ", n*10) // print something n * 10
+			token <- struct{}{}                     // put struct back in token chan to allow send() to continue processing
 		case <-done:
 			return
 		}
 	}
 }
 
-func setupTokens() {
+func setupTokens() { // adds tokens to token chan based on token chan length
 	for i := 0; i < len(token); i++ {
 		token <- struct{}{}
 	}
